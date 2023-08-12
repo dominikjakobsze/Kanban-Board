111717f1d5d7e6ecbf3457a0990db99e007518d4
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-08-12 21:25:52 +0200

    carousel initialized

--- frontend/app-vite/src/js/CarouselComponent.js
+++ frontend/app-vite/src/js/CarouselComponent.js
@@ -1,8 +1,12 @@
 import EmblaCarousel from 'embla-carousel';
 
-const emblaNode = document.querySelector('[data-carousel="main"]');
-const options = { loop: false };
-
-const emblaApi = EmblaCarousel(emblaNode, options);
-
-console.log(emblaApi.slideNodes());
+const carouselNode = document.querySelector('[data-carousel="main"]');
+const carouselOptions = { 
+    loop: false,
+    align: 'start',
+    axis: 'x',
+    dragFree: true,
+    watchDrag: true,
+ };
+const carouselApi = EmblaCarousel(carouselNode, carouselOptions);
+console.log(carouselApi.slideNodes(),"running");
