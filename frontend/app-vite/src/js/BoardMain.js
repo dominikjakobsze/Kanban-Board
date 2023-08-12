0001a5485d5f0e7f018280a7c16e609330fc44ab
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-08-12 22:42:12 +0200

    height column js fix

--- frontend/app-vite/src/js/BoardMain.js
+++ frontend/app-vite/src/js/BoardMain.js
@@ -1,4 +1,13 @@
 import { CarouselComponentInit, CarouselComponentKill, carouselApi } from './CarouselComponent.js';
 
+const HeightControl = () => {
+    const newColumn = document.querySelector('[data-column="new-column"]');
+    const newColumnHeight = newColumn.offsetHeight;
+    console.log(newColumnHeight);
+    const allColumns = document.querySelectorAll('[data-column="column"]');
+    console.log(allColumns);
+};
+
+HeightControl();
 CarouselComponentInit();
 console.log(carouselApi);
