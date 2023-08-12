8cab2dbe8f531a62a243004c1d20b4a936605222
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-08-12 21:36:56 +0200

    carousel api export

---
+++ frontend/app-vite/src/js/BoardMain.js
@@ -0,0 +1,4 @@
+import { CarouselComponentInit, CarouselComponentKill, carouselApi } from './CarouselComponent.js';
+
+CarouselComponentInit();
+console.log(carouselApi);
--- frontend/app-vite/src/js/CarouselComponent.js
+++ frontend/app-vite/src/js/CarouselComponent.js
@@ -1,12 +1,25 @@
 import EmblaCarousel from 'embla-carousel';
 
-const carouselNode = document.querySelector('[data-carousel="main"]');
-const carouselOptions = { 
-    loop: false,
-    align: 'start',
-    axis: 'x',
-    dragFree: true,
-    watchDrag: true,
- };
-const carouselApi = EmblaCarousel(carouselNode, carouselOptions);
-console.log(carouselApi.slideNodes(),"running");
+export let carouselApi = null;
+
+export const CarouselComponentInit = () => {
+    const carouselNode = document.querySelector('[data-carousel="main"]');
+    const carouselOptions = { 
+        loop: false,
+        align: 'start',
+        axis: 'x',
+        dragFree: true,
+        watchDrag: true,
+     };
+    carouselApi = EmblaCarousel(carouselNode, carouselOptions);
+    console.log(carouselApi.slideNodes(),"running");
+};
+
+export const CarouselComponentKill = () => {
+    carouselApi.destroy();
+    console.log("killed");
+};
+
+export const CarouselControls = () => {
+    
+};
