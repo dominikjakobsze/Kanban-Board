cec1298882b36b1ca5cf95e2005f029ad14f887b
Dominik Jakobsze <dominikjakobsze00@gmail.com>
2023-08-13 21:30:48 +0200

    refactor js #2

--- frontend/app-vite/src/js/SortableColumns.js
+++ frontend/app-vite/src/js/SortableColumns.js
@@ -1,9 +1,37 @@
-import Sortable from "sortablejs";
+import Sortable from 'sortablejs/modular/sortable.complete.esm.js';
 
 const columnsContainer = document.querySelector("[data-layout='right-panel-bottom']");
 
 new Sortable(columnsContainer, {
     animation: 150,
     handle: "[data-board='column']",
+    scroll: true,
+    forceAutoscrollFallback: false, 
+    scrollSensitivity: 30, 
+    scrollSpeed: 10, 
+    bubbleScroll: true,
+    group: {
+        name: 'columns',
+        pull: false, // whether dragging items from this group to add to another is allowed
+        put: false, // whether dropping on this group from another is allowed
+    },
 });
-console.log(columnsContainer);
+
+
+const tasksContainers = document.querySelectorAll("[data-board='column']");
+tasksContainers.forEach((tasksContainer) => {
+    new Sortable(tasksContainer, {
+        animation: 150,
+        handle: "[data-board='task']",
+        scroll: true,
+        forceAutoscrollFallback: false, 
+        scrollSensitivity: 30, 
+        scrollSpeed: 10, 
+        bubbleScroll: true,
+        group: {
+            name: 'tasks',
+            pull: true,
+            put: true,
+        }, 
+    });
+});
